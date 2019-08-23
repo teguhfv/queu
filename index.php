09394dbfb526b1dfb28ffe3631dbfd7b87ca5f50
IT Administrator <it@rsiaharapanbunda.com>
2019-08-23 16:57:49 +0800

    wiu wiu

--- index.php
+++ index.php
@@ -84,6 +84,25 @@
     <h1 class="font-weight-light mt-4 text-white">KAMAR TERSEDIA</h1>
     <p class="lead text-white-50">Use just two Bootstrap 4 utility classes and three custom CSS rules and you will have a flexbox enabled sticky footer for your website!</p>
     <i class="fa fa-bed" style="font-size:48px"></i> 
+    <i class="fa fa-bed" style="font-size:48px"></i> 
+    <i class="fa fa-bed" style="font-size:48px"></i> 
+    <i class="fa fa-bed" style="font-size:48px"></i> 
+    <i class="fa fa-bed" style="font-size:48px"></i> 
+    <i class="fa fa-bed" style="font-size:48px"></i> 
+    <i class="fa fa-bed" style="font-size:48px"></i> 
+    <i class="fa fa-bed" style="font-size:48px"></i> 
+    <i class="fa fa-bed" style="font-size:48px"></i> 
+    <i class="fa fa-bed" style="font-size:48px"></i> 
+    <i class="fa fa-bed" style="font-size:48px"></i> 
+    <i class="fa fa-bed" style="font-size:48px"></i> 
+    <i class="fa fa-bed" style="font-size:48px"></i> 
+    <i class="fa fa-bed" style="font-size:48px"></i> 
+    <i class="fa fa-bed" style="font-size:48px"></i> 
+    <i class="fa fa-bed" style="font-size:48px"></i> 
+    <i class="fa fa-bed" style="font-size:48px"></i> 
+    <i class="fa fa-bed" style="font-size:48px"></i> 
+    <i class="fa fa-bed" style="font-size:48px"></i> 
+    <i class="fa fa-bed" style="font-size:48px"></i> 
     </div>
     </div> 
     `,
@@ -121,18 +140,7 @@
     </div> 
     `,
     type: 'dom'
-  }, {
-    content: `       
-    <div class="row justify-content-center">
-    <div class="col-md-7">
-    <h1 class="font-weight-light mt-4 text-white">JADWAL DOKTER</h1>
-    <p class="lead text-white-50">Use just two Bootstrap 4 utility classes and three custom CSS rules and you will have a flexbox enabled sticky footer for your website!Use just two Bootstrap 4 utility classes and three custom CSS rules and you will have a flexbox enabled sticky footer for your website!Use just two Bootstrap 4 utility classes and three custom CSS rules and you will have a flexbox enabled sticky footer for your website!Use just two Bootstrap 4 utility classes and three custom CSS rules and you will have a flexbox enabled sticky footer for your website!Use just two Bootstrap 4 utility classes and three custom CSS rules and you will have a flexbox enabled sticky footer for your website!Use just two Bootstrap 4 utility classes and three custom CSS rules and you will have a flexbox enabled sticky footer for your website!Use just two Bootstrap 4 utility classes and three custom CSS rules and you will have a flexbox enabled sticky footer for your website!Use just two Bootstrap 4 utility classes and three custom CSS rules and you will have a flexbox enabled sticky footer for your website!Use just two Bootstrap 4 utility classes and three custom CSS rules and you will have a flexbox enabled sticky footer for your website!Use just two Bootstrap 4 utility classes and three custom CSS rules and you will have a flexbox enabled sticky footer for your website!Use just two Bootstrap 4 utility classes and three custom CSS rules and you will have a flexbox enabled sticky footer for your website!Use just two Bootstrap 4 utility classes and three custom CSS rules and you will have a flexbox enabled sticky footer for your website!Use just two Bootstrap 4 utility classes and three custom CSS rules and you will have a flexbox enabled sticky footer for your website!Use just two Bootstrap 4 utility classes and three custom CSS rules and you will have a flexbox enabled sticky footer for your website!Use just two Bootstrap 4 utility classes and three custom CSS rules and you will have a flexbox enabled sticky footer for your website!Use just two Bootstrap 4 utility classes and three custom CSS rules and you will have a flexbox enabled sticky footer for your website!Use just two Bootstrap 4 utility classes and three custom CSS rules and you will have a flexbox enabled sticky footer for your website!</p>
-    <i class="fa fa-bed" style="font-size:48px"></i> 
-    </div>
-    </div> 
-    `,
-    type: 'dom'
-  }
+  } 
   ], {
     animateType: 'card',
     isAutoplay: 1,
--- newsticker.php
+++ newsticker.php
@@ -1,5 +1,5 @@
-<div class="ti_news"><a href="#"><span>11:00</span> US fisherman rescued by tanker after 66 days lost at sea</a></div>
-<div class="ti_news"><a href="#"><span>12:00</span> Overseas aid must rise by £1bn in next two years, says Europe</a></div>
-<div class="ti_news"><a href="#"><span>13:00</span> Muslim population looks likely to double in size </a></div>
-<div class="ti_news"><a href="#"><span>15:00</span> Heathrow cuts passenger levy to boost domestic flights</a></div>
-<div class="ti_news"><a href="#"><span>16:00</span> Couple plotted to sell their new baby online for €5,000 </a></div>
+<div class="ti_news"><a href="#"><span>(DOKTER KANDUNGAN) </span> <strong>Dr.IG.N.AGUNG DWIPELITA PUTRA,Sp.OG</strong> </a></div>
+<div class="ti_news"><a href="#"><span>(DOKTER KANDUNGAN) </span> <strong>Dr.IG.N.AGUNG DWIPELITA PUTRA,Sp.OG</strong> </a></div>
+<div class="ti_news"><a href="#"><span>(DOKTER KANDUNGAN) </span> <strong>Dr.IG.N.AGUNG DWIPELITA PUTRA,Sp.OG</strong> </a></div>
+<div class="ti_news"><a href="#"><span>(DOKTER KANDUNGAN) </span> <strong>Dr.IG.N.AGUNG DWIPELITA PUTRA,Sp.OG</strong> </a></div>
+<div class="ti_news"><a href="#"><span>(DOKTER KANDUNGAN) </span> <strong>Dr.IG.N.AGUNG DWIPELITA PUTRA,Sp.OG</strong> </a></div>
